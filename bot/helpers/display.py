c30ebdfdd22039104382e8fd830d5a0e65631a61
AbhijithNT <abhijithnt64@gmail.com>
2021-12-07 21:16:08 +0530

    string space

--- bot/helpers/display.py
+++ bot/helpers/display.py
@@ -50,4 +50,4 @@ def human_readable_size(size, decimal_places=2):
         if size < 1024.0:
             break
         size /= 1024.0
-    return f"{size:.{decimal_places}f}{unit}"
+    return f"{size:.{decimal_places}f} {unit}"
