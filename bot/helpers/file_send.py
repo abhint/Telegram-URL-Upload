bd47ce67861a5cc4ac8ed8e4463ea4b4f8ceb0ae
AbhijithNT <abhijithnt64@gmail.com>
2021-12-07 14:00:15 +0530

    progress bar

--- bot/helpers/file_send.py
+++ bot/helpers/file_send.py
@@ -2,38 +2,63 @@ import magic
 from ..linktofile import TG
 from pyrogram.types import Message
 from pyrogram.errors import RPCError
+import time
+from .display import progress
 
 
-async def file_send(filename: str, client: TG, updates: Message):
+async def file_send(filename: str, client: TG, updates: Message, message: Message):
+    _now = time.time()
     file_mime = magic.Magic(mime=True).from_file(filename)
     try:
         if file_mime.startswith('video'):
             return await client.send_video(
                 chat_id=updates.chat.id,
-                reply_to_message_id=updates.message_id,
                 video=filename,
-                caption="Hello"
+                caption="Hello",
+                reply_to_message_id=message.message_id,
+                progress=progress,
+                progress_args=(
+                    updates,
+                    _now
+                )
             )
         elif file_mime.startswith('image'):
             return await client.send_photo(
                 chat_id=updates.chat.id,
-                reply_to_message_id=updates.message_id,
                 photo=filename,
-                caption="Hello"
+                caption="Hello",
+                reply_to_message_id=message.message_id,
+                progress=progress,
+                progress_args=(
+                    updates,
+                    _now
+                )
             )
+
         elif file_mime.startswith('audio'):
             return await client.send_audio(
                 chat_id=updates.chat.id,
-                reply_to_message_id=updates.message_id,
                 audio=filename,
-                caption="Hello"
+                caption="Hello",
+                progress=progress,
+                reply_to_message_id=message.message_id,
+                progress_args=(
+                    updates,
+                    _now
+                )
             )
         else:
             return await client.send_document(
                 chat_id=updates.chat.id,
-                reply_to_message_id=updates.message_id,
                 document=filename,
-                caption="Hello"
+                caption="Hello",
+                progress=progress,
+                reply_to_message_id=message.message_id,
+                progress_args=(
+                    updates,
+                    _now
+                )
             )
     except RPCError as err:
         raise Exception(f'{err}')
+    # await updates.delete()
