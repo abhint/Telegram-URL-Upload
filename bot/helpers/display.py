3d6eb689c36801e29e749f3d594daad13c4e1f91
AbhijithNT <abhijithnt64@gmail.com>
2021-12-19 16:12:16 +0530

    decimal

--- bot/helpers/display.py
+++ bot/helpers/display.py
@@ -47,8 +47,8 @@ def progress_bar(percent):
 
 
 def human_readable_size(size, decimal_places=2):
-    for unit in ['B', 'KiB', 'MiB', 'GiB', 'TiB']:
-        if size < 1024.0:
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if size < 1000.0:
             break
-        size /= 1024.0
+        size /= 1000.0
     return f"{size:.{decimal_places}f} {unit}"
