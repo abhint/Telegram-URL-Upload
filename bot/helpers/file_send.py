6a60e3c99dc0080108f75ae9f52f3b18466dbfae
AbhijithNT <abhijithnt64@gmail.com>
2021-12-20 13:50:58 +0530

    add video details (thumb ,duration)

--- bot/helpers/file_send.py
+++ bot/helpers/file_send.py
@@ -4,6 +4,7 @@ from pyrogram.types import Message
 from pyrogram.errors import RPCError
 import time
 from .display import progress
+from .tools import video_details
 
 
 async def file_send(filename: str, client: TG, updates: Message, message: Message):
@@ -11,12 +12,15 @@ async def file_send(filename: str, client: TG, updates: Message, message: Messag
     file_mime = magic.Magic(mime=True).from_file(filename)
     try:
         if file_mime.startswith('video'):
+            duration, thumb = video_details(filename)
             return await client.send_video(
                 chat_id=updates.chat.id,
                 video=filename,
                 caption=f"MIME: {file_mime}\nvideo",
                 reply_to_message_id=message.message_id,
                 progress=progress,
+                duration=duration,
+                thumb=thumb,
                 progress_args=(
                     updates,
                     _now
