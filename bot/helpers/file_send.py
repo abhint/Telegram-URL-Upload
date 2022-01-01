411507cfe5170f1910f4b4a1974f7993a1f63c35
AbhijithNT <abhijithnt64@gmail.com>
2022-01-01 17:22:32 +0530

    uploading message (#11)

--- bot/helpers/file_send.py
+++ bot/helpers/file_send.py
@@ -3,7 +3,8 @@ from ..linktofile import TG
 from pyrogram.types import Message
 from pyrogram.errors import RPCError
 import time
-from .display import progress
+from .time import time_data
+# from .display import progress
 from .tools import video_details
 
 
@@ -14,47 +15,54 @@ async def file_send(file_path: str, client: TG, updates: Message, message: Messa
     try:
         if file_mime.startswith('video'):
             width, height, duration, thumb = video_details(file_path)
-            return await client.send_video(
+            await updates.edit_text('Uploading...')
+            await client.send_video(
                 chat_id=updates.chat.id,
                 video=file_path,
                 caption=f"{file_name}",
                 reply_to_message_id=message.message_id,
-                progress=progress,
                 duration=duration,
                 thumb=thumb,
                 width=width,
                 height=height,
-                progress_args=(updates,)
+                # progress=progress,
+                # progress_args=(updates,)
             )
+            await updates.edit_text(f'Uploaded...100% in {time_data(time.time() - _now)}')
         elif file_mime.startswith('image'):
-            return await client.send_photo(
+            await updates.edit_text('Uploading...')
+            await client.send_photo(
                 chat_id=updates.chat.id,
                 photo=file_path,
                 caption=f"{file_name}",
                 reply_to_message_id=message.message_id,
-                progress=progress,
-                progress_args=(updates,)
+                # progress=progress,
+                # progress_args=(updates,)
 
             )
-
+            await updates.edit_text(f'Uploaded...100% in {time_data(time.time() - _now)}')
         elif file_mime.startswith('audio'):
-            return await client.send_audio(
+            await updates.edit_text('Uploading...')
+            await client.send_audio(
                 chat_id=updates.chat.id,
                 audio=file_path,
                 caption=f"{file_name}",
-                progress=progress,
                 reply_to_message_id=message.message_id,
-                progress_args=(updates,)
+                # progress=progress,
+                # progress_args=(updates,)
             )
+            await updates.edit_text(f'Uploaded...100% in {time_data(time.time() - _now)}')
         else:
-            return await client.send_document(
+            await updates.edit_text('Uploading...')
+            await client.send_document(
                 chat_id=updates.chat.id,
                 document=file_path,
                 caption=f"{file_name}",
-                progress=progress,
                 reply_to_message_id=message.message_id,
-                progress_args=(updates,),
+                # progress=progress,
+                # progress_args=(updates,),
             )
+            await updates.edit_text(f'Uploaded...100% in {time_data(time.time() - _now)}')
     except RPCError as err:
         raise Exception(f'{err}')
     # await updates.delete()
