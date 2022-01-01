b6816af27e7b75b8e7d9fb915eaff150e0958693
AbhijithNT <abhijithnt64@gmail.com>
2022-01-01 15:12:49 +0530

    bug (new line)

--- bot/env.py
+++ bot/env.py
@@ -10,7 +10,7 @@ def get_env(name: str, terminal_action: bool = True) -> str:
     if terminal_action:
         value = input(f'ENTER YOUR {name}: ')
         with open('.env', 'a') as env:
-            env.write(f'{name}={value}\n')
+            env.write(f'\n{name}={value}\n')
         return value
     else:
         return name
